bad18727fce396ee5f48a2ac9609a1888b1109d7
karamshuk@gmail.com <karamshuk@gmail.com@b731bd7c-ce30-6d3b-e675-ea99f7f392e3>
2010-07-19 20:27:27 +0000

    implemented FifoSimulation

---
+++ trunk/eclipse-project/src/org/imt/drr/model/HostType.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.imt.drr.model;
+
+/**
+ * 
+ * Just a simple enumarator for host types. 
+ * 
+ * @author Andrea Vandin, Dmytro Karamshuk, Iffat Ahmed 
+ *         PhD students at IMTLucca http://imtlucca.it
+ *
+ */
+public enum HostType {
+  CONSTANT_SIZE,
+  RANDOM_SIZE
+}
