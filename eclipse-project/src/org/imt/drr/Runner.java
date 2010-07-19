9c5a73d07f7c70dc06a6ddac51e795941cb508a8
karamshuk <karamshuk@gmail.com>
2010-07-19 15:27:26 +0000

    added log

--- eclipse-project/src/org/imt/drr/Runner.java
+++ eclipse-project/src/org/imt/drr/Runner.java
@@ -3,7 +3,13 @@
  */
 package org.imt.drr;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.imt.drr.model.fifo.FifoSimulator;
+
 /**
+ * The main application launch class.
+ * 
  * @author Andrea Vandin, Dmytro Karamshuk, Iffat Ahmed 
  *         PhD students at IMTLucca http://imtlucca.it
  *
@@ -11,12 +17,26 @@ package org.imt.drr;
 public class Runner {
 
   /**
+   * Logger
+   */
+  static Logger logger = Logger.getLogger(Runner.class);
+  
+  /**
+   * The main method of the app.
+   * 
    * @param args
    */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    //RandomData random = new RandomDataImpl();
-    //random.nextPoisson(1);
+    
+    //Configure logger
+    PropertyConfigurator.configure("log/log4j.properties");
+    
+    //We just start with creating simple Fifo Simulator
+    FifoSimulator simulator = new FifoSimulator();
+    simulator.initialize(100);
+    simulator.execute();
+    logger.debug("fuck off");
+    
   }
 
 }
