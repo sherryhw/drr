d00dd9c4b0ffa2909fc422cea0f1465938a561c8
karamshuk@gmail.com <karamshuk@gmail.com@b731bd7c-ce30-6d3b-e675-ea99f7f392e3>
2010-07-21 10:16:43 +0000

    updates to simulation

---
+++ trunk/eclipse-project/src/org/imt/drr/model/Constants.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.imt.drr.model;
+
+/**
+ * @author Andrea Vandin, Dmytro Karamshuk, Iffat Ahmed 
+ *         PhD students at IMTLucca http://imtlucca.it
+ *
+ */
+public final class Constants {
+
+  public static final int DEFAULT_PACKET_SIZE_MAX = 1000;
+  public static final int DEFAULT_NUMBER_OF_FLOWS = 2;//20;
+  public static final int DEFAULT_ARRIVAL_TIME_MEAN = 1;//100;
+  
+  public static final int MAXQUEUESIZE = 500;
+  public static final int DEFAULT_BANDWIDTH = 50;
+
+  public static final int DEFAULTQUANTUMOFSERVICE = 500;
+}
