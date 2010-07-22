e6659dfa146b42d8c11cf7486fb7648dba3bfdf2
karamshuk@gmail.com <karamshuk@gmail.com@b731bd7c-ce30-6d3b-e675-ea99f7f392e3>
2010-07-22 14:09:33 +0000

    continue with simulation

--- trunk/eclipse-project/src/org/imt/drr/model/Constants.java
+++ trunk/eclipse-project/src/org/imt/drr/model/Constants.java
@@ -10,29 +10,11 @@ package org.imt.drr.model;
  */
 public final class Constants {
 
-// ###############Configuration for 4 fifos##################
-//  public static final int DEFAULT_PACKET_SIZE_MAX = 4500;
-//  public static final int DEFAULT_NUMBER_OF_FLOWS = 20;
-//  public static final int DEFAULT_ARRIVAL_TIME_MEAN = 100;
-//  
-//  public static final int MAXQUEUESIZE = 500;
-//  public static final int DEFAULT_BANDWIDTH = 1680; //250;
-//
-//  public static final int DEFAULTQUANTUMOFSERVICE = 4000;
-//
-//  public static final boolean USE_LOG = false;
-//
-//  public static final int SETUP_TIME = 5000; //50000;
-//
-//  public static final int PART_AVERAGE = 100;
-//  
-//  public static final int SIMULATION_TIME = 100000;
-
   public static final boolean USE_BASTARD_FLOW = true;
   
   public static final int DEFAULT_PACKET_SIZE_MAX = 4500;
   public static final int DEFAULT_NUMBER_OF_FLOWS = 20;
-  public static final int DEFAULT_ARRIVAL_TIME_MEAN = 100;
+  public static final int DEFAULT_ARRIVAL_TIME_MEAN = 45;
   
   public static final int MAXQUEUESIZE = 10000; //20 * 500
   public static final int DEFAULT_BANDWIDTH = 1000; //250;
@@ -45,6 +27,28 @@ public final class Constants {
   
   public static final int PART_AVERAGE = 100;
   
-  public static final int SIMULATION_TIME = 1000000;
+  public static final int SIMULATION_TIME = 2000000;
+  
+  public static final int NUMBER_OF_RUNS = 5;
 
+  public static final String OUTPUT_FILE_NAME = "log/results.txt";
+
+  /**
+   * returns string representation
+   * @return
+   */
+  public static final String getConstantsString() {
+    String str = "";
+    str += "USE_BASTARD_FLOW = " + USE_BASTARD_FLOW + "\r\r\n";
+    str += "DEFAULT_PACKET_SIZE_MAX = " + DEFAULT_PACKET_SIZE_MAX + "\r\n";
+    str += "DEFAULT_NUMBER_OF_FLOWS = " + DEFAULT_NUMBER_OF_FLOWS + "\r\n";
+    str += "DEFAULT_ARRIVAL_TIME_MEAN = " + DEFAULT_ARRIVAL_TIME_MEAN + "\r\n";
+    str += "MAXQUEUESIZE = " + MAXQUEUESIZE + "\r\n";
+    str += "DEFAULT_BANDWIDTH = " + DEFAULT_BANDWIDTH + "\r\n";
+    str += "SETUP_TIME = " + SETUP_TIME + "\r\n";
+    str += "PART_AVERAGE = " + PART_AVERAGE + "\r\n";
+    str += "SIMULATION_TIME = " + SIMULATION_TIME + "\r\n\r\n";
+    str += "NUMBER_OF_RUNS = " + NUMBER_OF_RUNS + "\r\n\r\n";
+    return str;
+  }
 }
--- trunk/eclipse-project/src/org/imt/drr/simulators/StatsWriter.java
+++ trunk/eclipse-project/src/org/imt/drr/simulators/StatsWriter.java
@@ -1,19 +1,39 @@
 package org.imt.drr.simulators;
 
 import java.io.*;
-import java.util.Collection;
 import java.util.Vector;
 
-public class StatsWriter {
+import org.imt.drr.model.Constants;
 
-  public static void writeResults(String fileName, Vector<Integer> flowIds, Vector<Double> throughput){
+public final class StatsWriter {
+
+  public static void writeHeader(String fileName) {
+    try{
+      FileWriter fstream = new FileWriter(fileName, false);
+      BufferedWriter out = new BufferedWriter(fstream);
+      out.write("#######################################################################\r\n\r\n");
+      out.write(Constants.getConstantsString());
+      out.write("#######################################################################\r\n\r\n");
+      out.write("\r\n");
+      //Close the output stream
+      out.close();
+    }catch (Exception e){//Catch exception if any
+      System.err.println("Error: " + e.getMessage());
+    }
+    
+  }
+
+  public static void writeResults(String fileName, double[] throughput, int experimentNumber){
     try{
       // Create file 
-      FileWriter fstream = new FileWriter(fileName);
+      FileWriter fstream = new FileWriter(fileName, true);
       BufferedWriter out = new BufferedWriter(fstream);
-      for(int i=0; i < flowIds.size();i++){
-        out.write(flowIds.get(i) +" "+throughput+"\n");
+      out.write("######################## EXPERIMENT RUN N" + experimentNumber + " #######################\r\n\r\n");
+//      out.write(Constants.getConstantsString());
+      for(int i = 0; i < throughput.length; i++) {
+        out.write(i + " " + throughput[i] + "\r\n");
       }
+      out.write("\r\n");
       //Close the output stream
       out.close();
     }catch (Exception e){//Catch exception if any
